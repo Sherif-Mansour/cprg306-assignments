46e48cf8d3fefce23e1e2d0731a75f7c4d335be0
Sherif-Mansour <135798032+Sherif-Mansour@users.noreply.github.com>
2024-01-21 10:47:05 -0700

    Create StudentInfo, Link to week-2, and Link to root

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+        <main>
+            <h1>Sherif Mansour</h1>
+            <ul>
+                <li>
+                    <Link href="https://github.com/Sherif-Mansour/cprg306-assignments">Github Repository</Link>
+                </li>
+            </ul>
+        </main>
+    );
+}
